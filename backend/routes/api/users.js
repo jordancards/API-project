ec818a8cb81cc7c8048b58d3cc31cfafa4e5f197
Jordan <jordancardinez@gmail.com>
2024-02-23 10:03:51 -0800

    adjusted migrations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -33,19 +33,24 @@ const validateSignup = [
 ];
 
 // Sign up
-router.post(
-  '/',
-  validateSignup,
-  async (req, res) => {
-    const { email, password, username } = req.body;
-    const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({ email, username, hashedPassword });
+router.post("/", validateSignup, async (req, res) => {
+  const { firstName, lastName, email, password, username } = req.body;
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    username,
+    hashedPassword,
+  });
 
-    const safeUser = {
-      id: user.id,
-      email: user.email,
-      username: user.username,
-    };
+  const safeUser = {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    username: user.username,
+  };
 
     await setTokenCookie(res, safeUser);
 
