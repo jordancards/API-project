12bcf61d1add8da093f4da68812e845c8e4b046d
Jordan <jordancardinez@gmail.com>
2024-02-23 09:17:31 -0800

    render adjustments

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,6 +10,29 @@ const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 // Sign up
+
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+// Sign up
 router.post(
   '/',
   validateSignup,
@@ -31,24 +54,4 @@ router.post(
     });
   }
 );
-
-const validateSignup = [
-  check('email')
-    .exists({ checkFalsy: true })
-    .isEmail()
-    .withMessage('Please provide a valid email.'),
-  check('username')
-    .exists({ checkFalsy: true })
-    .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
-  check('username')
-    .not()
-    .isEmail()
-    .withMessage('Username cannot be an email.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
-  handleValidationErrors
-];
 module.exports = router;
