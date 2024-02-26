a4ebcac903bfdec41bf285b0377aa04dd5fc36b9
Jordan <jordancardinez@gmail.com>
2024-02-26 11:09:53 -0800

    spots model/migration

--- backend/db/migrations/20240226185247-create-review.js
+++ backend/db/migrations/20240226185247-create-review.js
@@ -16,13 +16,20 @@ module.exports = {
       spotId: {
         type: Sequelize.INTEGER,
         references: {
+          model:'Spots',
+          key: 'id'
         },
-
+        onDelete:'CASCADE'
       },
-      userId: {
+        userId: {
         type: Sequelize.INTEGER,
+        references:{
+          model:'Users',
+          key: 'id'
+        },
+        onDelete:'CASCADE'
       },
-    
+
       review: {
         type: Sequelize.STRING,
         allowNull:false
